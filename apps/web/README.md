4fb745bc2b877f9657ecd11f29697054f0ec2460
dukc <dukkcc@proton.me>
2024-08-30 21:34:53 -0700

    rewrite, and new license!

--- apps/web/README.md
+++ apps/web/README.md
@@ -1,30 +1,36 @@
-![Web app banner](https://i.imgur.com/R1HNhih.png)
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-# Bibleio Web App
+## Getting Started
 
-The Bibleio web app is a simple, casual reading experience with basic text formatting features.
+First, run the development server:
 
-Visit the release version [here](https://app.bibleio.com).
-
-# Development
-
-Clone the main "bibleio/apps" monorepo, then run:
 ```bash
-pnpm install
-pnpm web:dev
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
 ```
-Currently, we use [scripture.api.bible](https://scripture.api.bible/) for the API. Once we get our API running, we'll migrate. For now, you will need your own API key to use scripture.api.bible. Create your own app [here](https://scripture.api.bible/signup), then copy your key. Then create a `.env` file inside the root, and put inside:
 
-```env
-API_KEY="YOUR_API_KEY_HERE"
-```
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-# Plans
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
-- [x] Text formatting (fonts, size, line-spacing)
-- [ ] Themes (background, accent)
-- [ ] Bookmarks, highlighting
-- [ ] Notetaking
-- [ ] Prayer list
-- [ ] Accounts to store above saved data
-- [ ] Split view to compare translations?
+This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,38 +1,40 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div className="flex flex-col py-64 px-256 gap-16">
-      <h1 className="h1">Bibleio</h1>
-      <p className="body-paragraph">
-        Bibleio is an app focused on making the Bible accessible, easy to
-        understand, and spreading the Gospel.
-      </p>
-      <h3 className="h3">Beta</h3>
-      <p className="body-paragraph">
-        This app is still in beta stages. Currently, the base functionality
-        (accessing the Bible) and some text formatting is all this does. Next, I
-        plan to add a dictionary define system, a command pallete styled search
-        function, bookmarks, more themes, an entire page with cool animations
-        showing the reasons why you should believe, and more!
-      </p>
-      <p className="body-paragraph">
-        This is 100% free & open-source. Made by dukc.
-      </p>
-      <div className="flex gap-16">
-        <a
-          href="/bible"
-          className=" bg-light-accent-navy border border-black rounded-12 shadow-accent-component component-hover-effect text-white px-64 py-8"
-        >
-          Go to the app
-        </a>
-
-        <a
-          href="https://github.com/bibleio/bibleio"
-          target="_blank"
-          className=" bg-light-fg-2 border border-black rounded-12 shadow-component component-hover-effect text-black px-64 py-8"
-        >
-          GitHub
-        </a>
+    <main className="min-h-screen bg-light-bg-1 p-32">
+      <h1 className="text-h1 font-semibold">Welcome</h1>
+      <div className="grid grid-cols-4 gap-12">
+        <Card link="/bible">
+          <h3 className="text-h3 font-medium">Bible</h3>
+          <p className="text-body leading-none">Read the Bible.</p>
+        </Card>
+        <Card link="/pray">
+          <h3 className="text-h3 font-medium">Pray</h3>
+          <p className="text-body leading-none">View your prayer list.</p>
+        </Card>
+        <Card link="/testimonies">
+          <h3 className="text-h3 font-medium">Testimonies</h3>
+          <p className="text-body leading-none">
+            Look at other people's testimonies.
+          </p>
+        </Card>
+        <Card link="/settings/account">
+          <h3 className="text-h3 font-medium">Account</h3>
+          <p className="text-body leading-none">See your account data.</p>
+        </Card>
       </div>
-    </div>
+    </main>
+  );
+}
+
+function Card({ children, link }: { children: React.ReactNode; link: string }) {
+  return (
+    <Link
+      href={link}
+      className="rounded-16 border border-light-stroke bg-light-fg-1 p-32 duration-150 ease-out hover:-translate-y-4 hover:drop-shadow-md"
+    >
+      {children}
+    </Link>
   );
 }
